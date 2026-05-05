1caa0038271c77c546c85e8ac2f42d61834f2da9
Marina Sánchez Moreno <72214110+MarinaSM95@users.noreply.github.com>
2026-05-05 18:02:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,23 @@ menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
+<script>
+function showMap(type) {
+    // ocultar todos los mapas
+    document.querySelectorAll('.map-iframe').forEach(el => {
+        el.classList.remove('active');
+    });
+
+    // mostrar el seleccionado
+    document.getElementById('map-' + type).classList.add('active');
+
+    // actualizar botones
+    document.querySelectorAll('.map-btn').forEach(btn => {
+        btn.classList.remove('active');
+    });
+
+    event.target.classList.add('active');
+}
+</script>
 
 
